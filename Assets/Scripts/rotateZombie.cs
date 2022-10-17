a212184963a04db9bae8f0a8d19b0786bd73a016
SALTU-19 <69312350+SALTU-19@users.noreply.github.com>
2022-10-18 00:14:24 +0300

    first comment

---
+++ Assets/Scripts/rotateZombie.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class rotateZombie : MonoBehaviour
+{
+
+    public GameObject vbBtnObj;
+    public GameObject zombie;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("RotateZombie");
+        zombie = GameObject.Find("Zombie1");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //Vector3 temp = new Vector3(0, Time.deltaTime * 1000, 0);
+        //zombie.transform.position += temp;
+        zombie.transform.Rotate(new Vector3(0, Time.deltaTime * 100000, 0));
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Button released");
+    }
+}
